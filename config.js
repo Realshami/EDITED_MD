19a637d40aff0aa967d340c8605606fb031299d6
Realshami <154044925+Realshami@users.noreply.github.com>
2025-08-08 08:19:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~KMM9Rrowk36e2PL", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "KMM9Rrowk36e2PL", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb+srv://Saka:Saka@cluster0.jadwggo.mongodb.net/?retryWrites=true&w=majority",  //ඔයාගෙ mongoDb url එක
 };
  
